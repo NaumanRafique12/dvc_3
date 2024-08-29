311414be922ff1287da017607ebe22a7c9628185
naumanrafique12 <naumanrafique10.com>
2024-08-29 17:29:38 +0500

    first

---
+++ src/app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import os
+
+df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
+
+d = pd.DataFrame()
+d['first'] = df[['Length of Membership']]
+
+d.to_csv(os.path.join('data','customer.csv'))
