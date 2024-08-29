5fd3b12c61473a77301a0f9582605013eba3bf60
naumanrafique12 <naumanrafique10.com>
2024-08-29 17:31:12 +0500

    second

--- src/app.py
+++ src/app.py
@@ -5,6 +5,6 @@ import os
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
 
 d = pd.DataFrame()
-d['first'] = df[['Length of Membership']]
+d['second'] = df[['Length of Membership']]
 
 d.to_csv(os.path.join('data','customer.csv'))
